624ca9102d55295fc460b6ae536c45a2f5213759
rjatin007 <rjatin1991@gmail.com>
2025-08-13 18:38:28 +0530

    fixes:[WIP] Add fixes to components

--- src/components/ui/Collapsable/Collapsable.tsx
+++ src/components/ui/Collapsable/Collapsable.tsx
@@ -4,7 +4,7 @@ import ArrowDownIcon from "../../../assets/arrow-down-icon.svg";
 
 const Collapsable = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex flex-col justify-start items-start w-full bg-white p-4 border-b-2 border-solid border-slate-200">
+    <div className="flex flex-col justify-start items-start w-full bg-white p-4 border-b-1 border-solid border-slate-200">
       <div className="flex justify-between items-center w-full">
         <p className="font-semibold">Title</p>
         <img src={ArrowDownIcon} alt="Arrow Down Icon" className="size-6" />
--- src/components/ui/Modal/ModalContent.tsx
+++ src/components/ui/Modal/ModalContent.tsx
@@ -12,11 +12,11 @@ const ModalContent = ({ children }: { children: ReactNode }) => {
           className="size-10 bg-slate-100 rounded-full p-2"
         />
       </div>
-      <div className="flex flex-col justify-start items-start w-full max-md:max-h-[600px] overflow-y-scroll">
+      <div className="flex flex-col justify-start items-start w-full max-md:max-h-[600px] overflow-y-scroll overflow-x-hidden">
         {children}
       </div>
 
-      <div className="flex justify-center items-center w-full py-3 px-2 bg-white">
+      <div className="flex justify-center items-center w-full py-3 px-2 bg-white shadow-xl border-t-1 border-solid border-slate-200">
         <p className="w-full bg-black text-lg text-white font-semibold text-center px-4 py-3 rounded-2xl">
           View Results
         </p>
